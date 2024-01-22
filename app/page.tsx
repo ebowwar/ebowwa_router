2599ee80dd7c302c393a0bda62cbdaec94e42dfe
ebowwar <157273085+ebowwar@users.noreply.github.com>
2024-01-22 04:52:20 +0000

    pray

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src="/concrete_guy.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
