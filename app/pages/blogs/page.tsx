1787c844a56a55c2164410f7a8531a3cfedb475d
ebowwar <157273085+ebowwar@users.noreply.github.com>
2024-01-24 00:23:58 +0000

    hope lmao

--- app/pages/blogs/page.tsx
+++ app/pages/blogs/page.tsx
@@ -1,14 +1,14 @@
 "use client"; // Indicates this file uses client-side React features
 
 import React from 'react';
-import Container from '../../components/container';
-import MoreStories from '../../components/more-stories';
-import HeroPost from '../../components/hero-post';
-import Intro from '../../components/intro';
-import Layout from '../../components/layout';
+import Container from '../../../components/container';
+import MoreStories from '../../../components/more-stories';
+import HeroPost from '../../../components/hero-post';
+import Intro from '../../../components/intro';
+import Layout from '../../../components/layout';
 import Head from 'next/head';
-import { CMS_NAME } from '../../lib/constants';
-import Post from '../../interfaces/post';
+import { CMS_NAME } from '../../../lib/constants';
+import Post from '../../../interfaces/post';
 import styles from '../styles/Tags.module.css'; // Import the CSS module for tags
 import { getAllPosts } from '@/lib/api';
 
