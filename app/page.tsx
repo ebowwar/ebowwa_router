c1a9cbe3392c5c9d605cc33489e887d70b413c62
ebowwar <157273085+ebowwar@users.noreply.github.com>
2024-01-25 23:33:55 +0000

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -40,13 +40,13 @@ export default function Home() {
         </a>
 
         <a
-          href="/blogs"
+          href="/a-cell.vercel.app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-             blog{" "}
+            blog{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
